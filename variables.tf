ada45cb579df58fd3cccdc6be95ca3390d14b0ba
Christopher <motescj@aim.com>
2020-01-17 14:40:25 +0000

    add writing resource arn variable

--- variables.tf
+++ variables.tf
@@ -1,6 +1,13 @@
-#variable environment {
-#    type = string
-#}
+variable environment {
+    type = string
+    default = "prod"
+    description = "The environment the resource will exist"
+}
+
+variable writingResourceARN {
+    type = string
+    description = "the AWS resource that writes to the bucket"
+}
 
 #variable bucketName {
 #    type = string
