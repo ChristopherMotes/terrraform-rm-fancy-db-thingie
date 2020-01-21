c7d2db711a7a1f1e23df9a992a65a23011c46b27
Christopher <motescj@aim.com>
2020-01-21 14:42:36 +0000

    change to data call with value metadata

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "s3bucket" {
   source = "github.com/ChristopherMotes/terraform-module-s3-drop-off"
   bucketName = "thisismybucket"
+  loggingBucketName = data.aws_cloudformation_export.loggingBucketName.value
   writingResourceARN = var.writingResourceARN
 }
